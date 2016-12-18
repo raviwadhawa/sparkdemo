173053e4c39fe7d8608aa596480ca456ac010bad
raviwadhawa <raviwadhawait@gmail.com>
2016-12-18 16:56:26 +0530

    dhyanendra start work from here

--- src/demo/Helloworld.java
+++ src/demo/Helloworld.java
@@ -1,7 +1,8 @@
 package demo;
 
 public class Helloworld {
-public static void main ( String args[])
-{System.out.println("Hello world modification");
+	public static void main(String args[]) {
+		System.out.println("Hello world modification");
+		System.out.println("hello world working");
 	}
 }
